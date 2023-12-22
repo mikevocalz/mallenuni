1bf814de1bb4a519a9e987568187a925fb365936
Michael Allen <mikefacesny@gmail.com>
2023-12-22 12:19:18 -0500

    fix build error

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>5th Galaxy Studio's</title>
+        <title>5th Galaxy Studios</title>
         <meta
           name="description"
           content="Unlock innovation with 5th Galaxy Studio's, your trusted partner in software development. Our skilled team specializes in crafting cutting-edge iOS, Android, and web applications that redefine user experiences. From seamless cross-platform solutions to tailored app experiences, we deliver excellence in every line of code. Collaborate with us to turn your digital vision into reality."
