635218fa487348a423d289f20e9e28638dfedd9a
Michael Allen <mikefacesny@gmail.com>
2023-12-22 12:14:53 -0500

    update work screen color

--- packages/app/features/work/screen.tsx
+++ packages/app/features/work/screen.tsx
@@ -1,14 +1,25 @@
 
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Platform } from 'react-native'
+import { Section, Div, Article, P, Span, H2, H4, BR } from '@expo/html-elements'
 
+const isWeb = Platform.OS === 'web'
 export function WorkScreen() {
 
 
   return (
-    <View className='flex-1 h-full items-center min-h-screen self-center w-screen max-w-7xl  bg-red-500 p-4'>
-      <Text>Work</Text>
+    <View
+      className='flex-1 items-center min-h-screen max-w-7xl w-full bg-black '>
+      <ScrollView className='pb-[300px]'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: !isWeb ? 200 : null,
+          paddingTop: !isWeb ? 40 : null
+        }}
+        contentContainerClassName='flex-1 w-full bg-black '>
 
+        <H2 className="mb-6 text-3xl text-white text-center font-bold">Portfolio Showcase</H2>
 
+      </ScrollView>
     </View >
   )
 }
